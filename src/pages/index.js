d8a043428fba66f6a821306e23c9613769742fdd
WeijieFu <weijiefu92@gmail.com>
2021-03-16 16:23:03 +0100

    add path name

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,25 @@ import * as React from "react"
 // markup
 const IndexPage = (props) => {
   console.log(props)
+  const code =''
+  useEffect(() => {
+    const query = props.location.search
+    const start = props.location.search.search('=')
+    const end = props.location.search.search('&')
+    code = props.location.search[start, end]
+    const url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx08a65539b5bf483e&secret=80a0e3670fd65363c7690f96da7a9441&code=${code}&grant_type=authorization_code`
+   
+  }, [])
+  
+
+  
   return (
     <main >
       <title>Home Page</title>
       <h1>Hello this is NPC Game</h1>
 
 
-      <p>{props.location.search}</p>
+      <p>{code}</p>
       
 
       
