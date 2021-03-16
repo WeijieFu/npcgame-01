53c1f635092f0ea2b98069666aeab03c93f208fa
WeijieFu <weijiefu92@gmail.com>
2021-03-16 16:51:38 +0100

    add path name

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,32 @@
 import * as React from "react"
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
 
 // markup
 const IndexPage = (props) => {
   console.log(props)
+  const [userInfo, setUserInfo] = useState({nickname: '', headimgurl: '' })
+
   let code =''
   useEffect(() => {
     const query = props.location.search
     const start = query.search('=')
     const end = query.search('&')
     code = props.location.search.slice(start+1, end)
-    const url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx08a65539b5bf483e&secret=80a0e3670fd65363c7690f96da7a9441&code=${code}&grant_type=authorization_code`
-    console.log(code)
+    const tokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx08a65539b5bf483e&secret=80a0e3670fd65363c7690f96da7a9441&code=${code}&grant_type=authorization_code`
+    
+
+    const tokenRes = axios.get(tokenUrl)
+    const access_token = tokenRes.access_token
+    const openid = tokenRes.openid
+
+
+    const userInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`
+
+    const userInfoRes = axios.get(userInfoUrl)
+    setUserInfo({nickname: userInfoRes.nickname, headimgurl: userInfoRes.headimgurl})
+
   }, [])
   
 
@@ -23,7 +37,9 @@ const IndexPage = (props) => {
       <h1>Hello this is NPC Game</h1>
 
       <p>{props.location.search}</p>
-      <p>{code}</p>
+      <p>{userInfo.nickname}</p>
+      <p>{userInfo.headimgurl}</p>
+      
       
 
       
