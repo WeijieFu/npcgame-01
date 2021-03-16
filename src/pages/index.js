1e4ce803ce148f12c7895f7f260cd68822ad727c
WeijieFu <weijiefu92@gmail.com>
2021-03-16 16:10:53 +0100

    add path name

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,16 @@ import * as React from "react"
 
 
 // markup
-const IndexPage = () => {
+const IndexPage = (props) => {
+  console.log(props)
   return (
     <main >
       <title>Home Page</title>
       <h1>Hello this is NPC Game</h1>
 
 
-
+      <p>{props.location.pathname}</p>
+      
 
       
     </main>
