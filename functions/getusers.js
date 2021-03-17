603f2f3338d67c5748f0277568eb6411751d788b
WeijieFu <weijiefu92@gmail.com>
2021-03-17 12:01:27 +0100

    add lambda function

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,23 +1,23 @@
 const axios = require('axios')
 
 exports.handler = function(event, context, callback){
-    const {code} = JSON.parse(event.body)
-    let userInfoRes
-    
+    const {code} = JSON.parse(event.data)
+
+    console.log(code)
 
     const tokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx08a65539b5bf483e&secret=80a0e3670fd65363c7690f96da7a9441&code=${code}&grant_type=authorization_code`
     
 
     axios.get(tokenUrl).then(
         (tokenRes)=>{
-            const access_token = tokenRes.access_token
-            const openid = tokenRes.openid
+            const access_token = tokenRes.data.access_token
+            const openid = tokenRes.data.openid
             const userInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`
 
             axios.get(userInfoUrl).then( (userInfoRes)=>{
                 callback(null, {
                     statusCode: 200,
-                    body: JSON.stringify(userInfoRes)
+                    body: JSON.stringify(userInfoRes.data)
                 })
             }
                 
