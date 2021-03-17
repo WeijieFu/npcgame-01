5f4e34ae02d5b253199171193044903d469e246e
WeijieFu <weijiefu92@gmail.com>
2021-03-17 12:33:55 +0100

    add openid

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 // markup
 const IndexPage = (props) => {
   console.log(props)
-  const [userInfo, setUserInfo] = useState({nickname: '', headimgurl: '' })
+  const [userInfo, setUserInfo] = useState({nickname: '', headimgurl: '', openid: '' })
 
 
   useEffect(() => {
@@ -16,24 +16,8 @@ const IndexPage = (props) => {
     const code = props.location.search.slice(start+1, end)
 
     axios.post('/.netlify/functions/getusers', {"code" : code },{ headers: { 'Content-Type': 'application/json'}}).then(
-      (userInfoRes) => setUserInfo({nickname: userInfoRes.data.nickname, headimgurl: userInfoRes.data.headimgurl})
+      (userInfoRes) => setUserInfo({nickname: userInfoRes.data.nickname, headimgurl: userInfoRes.data.headimgurl, openid: userInfoRes.data.openid})
     )
-
-    // console.log(res)
-    
-    // const tokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx08a65539b5bf483e&secret=80a0e3670fd65363c7690f96da7a9441&code=${code}&grant_type=authorization_code`
-    
-
-    // const tokenRes = axios.get(tokenUrl)
-    // const access_token = tokenRes.access_token
-    // const openid = tokenRes.openid
-
-
-    // const userInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`
-
-    // const userInfoRes = axios.get(userInfoUrl)
-    // setUserInfo({nickname: userInfoRes.nickname, headimgurl: userInfoRes.headimgurl})
-
   }, [])
   
 
@@ -45,6 +29,7 @@ const IndexPage = (props) => {
 
     
       <p>your nickname is {userInfo.nickname}</p>
+      <p>your openid is {userInfo.openid}</p>
       <img src={userInfo.headimgurl} alt=""/>
       
       
