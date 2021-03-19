ed16419b453fa119e7cbe3694d30253d26f71693
WeijieFu <weijiefu92@gmail.com>
2021-03-19 15:28:51 +0100

    set up login

--- src/api/player.js
+++ src/api/player.js
@@ -2,17 +2,17 @@ import axios from 'axios'
 
 
 ///playerLogin
-export const playerLogin = async (openid)=>{
-    await axios.get(`https://training-game-strapi.herokuapp.com/players/${openid}`).then(
+export const playerLogin = (openid)=>{
+    axios.get(`https://training-game-strapi.herokuapp.com/players/${openid}`).then(
         // if user has an account -> login
         res => res.data
     ).catch(err => {
 
         // if user doesn't have an account -> create account 
-        await axios.post('https://training-game-strapi.herokuapp.com/players', {
-            openid: openid,
-        }).then(
-            res => res.data )
+    axios.post('https://training-game-strapi.herokuapp.com/players', {
+        openid: openid,
+    }).then(
+        res => res.data )
     }
         )
    
