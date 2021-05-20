f3e06f0ddaa2d5e86ba3fec1683481a2944cdbf3
WeijieFu <weijiefu92@gmail.com>
2021-05-20 11:27:20 +0200

    add .env

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,40 +1,32 @@
-const axios = require('axios')
+const axios = require("axios");
+require("dotenv").config();
 
-exports.handler = function(event, context, callback){
-    const {code} = JSON.parse(event.body)
+exports.handler = function (event, context, callback) {
+  const { code } = JSON.parse(event.body);
 
-    console.log(code)
+  console.log(code);
 
-    const tokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx08a65539b5bf483e&secret=80a0e3670fd65363c7690f96da7a9441&code=${code}&grant_type=authorization_code`
-    
+  const tokenUrl = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${process.env.APP_ID}&secret=${process.env.APP_SECRET}&code=${code}&grant_type=authorization_code`;
 
-    axios.get(tokenUrl).then(
-        (tokenRes)=>{
-            const access_token = tokenRes.data.access_token
-            const openid = tokenRes.data.openid
-            const userInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`
+  axios.get(tokenUrl).then((tokenRes) => {
+    const access_token = tokenRes.data.access_token;
+    const openid = tokenRes.data.openid;
+    const userInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`;
 
-            axios.get(userInfoUrl).then( (userInfoRes)=>{
-                callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(userInfoRes.data)
-                })
-            }
-                
-            )
-        }
-    )
-   
+    axios.get(userInfoUrl).then((userInfoRes) => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(userInfoRes.data),
+      });
+    });
+  });
 
+  // const userInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`
 
-    // const userInfoUrl = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`
+  // userInfoRes = axios.get(userInfoUrl)
 
-    // userInfoRes = axios.get(userInfoUrl)
-
-    
-
-    // callback(null, {
-    //     statusCode: 200,
-    //     body: JSON.stringify(userInfoRes)
-    // });
-}
+  // callback(null, {
+  //     statusCode: 200,
+  //     body: JSON.stringify(userInfoRes)
+  // });
+};
