4101364397f09e4439b76dc6b1126e64991f7101
WeijieFu <weijiefu92@gmail.com>
2021-06-16 13:17:39 +0200

    add character filter

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,7 @@ const IndexPage = (props) => {
       <title>寻找Aptamil有机星球</title>
       {page == "start" && <Start setPage={setPage} player={player} />}
       {page == "choosecharacter" && (
-        <ChooseCharacter setPage={setPage} player={setPlayer} />
+        <ChooseCharacter setPage={setPage} setPlayer={setPlayer} />
       )}
       {page == "chooselevel" && <ChooseLevel setPage={setPage} />}
       {page == "level1" && <Level1 setPage={setPage} />}
