d197dbc8f0504cef526cea773cd70fce283d3a2e
WeijieFu <weijiefu92@gmail.com>
2021-06-23 10:48:51 +0200

    add loading screen

--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import "../styles/loading.css";
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="loading">
+      <div className="loading__wrapper">
+        <img
+          className="start__background"
+          src="https://res.cloudinary.com/duykdzv1k/image/upload/v1616420398/background_3x_b623c176b6.png"
+          alt="background"
+        />
+        <img
+          className="loading__loader"
+          src="https://res.cloudinary.com/duykdzv1k/image/upload/v1623330596/planet_cow_3x_5bc9942397.png"
+          alt="loader"
+        />
+        <div className="loading__text">Loading</div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
